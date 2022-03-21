bc5f7d32af43f9ec05fc45679f50d705e36e779d
kungfuflex <judoflexchop@gmail.com>
2022-03-21 14:13:19 -0500

    fix proto encoding

--- .replrc.js
+++ .replrc.js
@@ -1,6 +1,8 @@
 
 var { QuPeer } = require('./');
+var sdk = require('./');
 var { enableGlobalMockRuntime } = require('./lib/mock');
+var proto = require('./lib/proto');
 
 var ethers = require('ethers');
 var wallet = ethers.Wallet.createRandom().connect(new ethers.providers.JsonRpcProvider('http://localhost:8545'));

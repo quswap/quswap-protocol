3b3e32da19474952d4238a55994658b46cee3f86
kungfuflex <judoflexchop@gmail.com>
2022-03-21 10:53:21 -0500

    update to get working with mocks

--- .replrc.js
+++ .replrc.js
@@ -1,2 +1,8 @@
 
 var { QuPeer } = require('./');
+var { enableGlobalMockRuntime } = require('./lib/mock');
+
+var ethers = require('ethers');
+var wallet = ethers.Wallet.createRandom().connect(new ethers.providers.JsonRpcProvider('http://localhost:8545'));
+var PeerId = require('peer-id');
+var { cryptoFromSeed } = require('./lib/id');

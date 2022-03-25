802cb9bb43baafe7334cf7b39c8e0cc4c8031c68
kungfuflex <judoflexchop@gmail.com>
2022-03-25 10:01:23 -0500

    fix deterministic seed generation

--- .replrc.js
+++ .replrc.js
@@ -5,6 +5,6 @@ var { enableGlobalMockRuntime } = require('./lib/mock');
 var proto = require('./lib/proto');
 
 var ethers = require('ethers');
-var wallet = ethers.Wallet.createRandom().connect(new ethers.providers.JsonRpcProvider('http://localhost:8545'));
+var wallet = new ethers.Wallet(ethers.utils.solidityKeccak256(['string'], ['dontusethis'])).connect(new ethers.providers.JsonRpcProvider('http://localhost:8545'));
 var PeerId = require('peer-id');
 var { cryptoFromSeed } = require('./lib/id');

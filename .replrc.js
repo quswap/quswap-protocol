1a90ccefffb8b179b9d6b36247ae150f535a36ed
kungfuflex <judoflexchop@gmail.com>
2022-03-29 07:59:41 -0500

    fix libp2p

--- .replrc.js
+++ .replrc.js
@@ -1,10 +1,21 @@
+var { QuPeer } = require("./");
+var sdk = require("./");
+var { enableGlobalMockRuntime } = require("./lib/mock");
+var proto = require("./lib/proto");
 
-var { QuPeer } = require('./');
-var sdk = require('./');
-var { enableGlobalMockRuntime } = require('./lib/mock');
-var proto = require('./lib/proto');
-
-var ethers = require('ethers');
-var wallet = new ethers.Wallet(ethers.utils.solidityKeccak256(['string'], ['dontusethis'])).connect(new ethers.providers.JsonRpcProvider('http://localhost:8545'));
-var PeerId = require('peer-id');
-var { cryptoFromSeed } = require('./lib/id');
+var ethers = require("ethers");
+var wallet = new ethers.Wallet(
+  ethers.utils.solidityKeccak256(["string"], ["dontusethis2"])
+).connect(new ethers.providers.JsonRpcProvider("http://localhost:8545"));
+var PeerId = require("peer-id");
+var { cryptoFromSeed } = require("./lib/id");
+var go = async () => {
+  const qu = await QuPeer.fromPassword({
+    signer: wallet,
+    password: "woop",
+  });
+  qu.on("peer:discovery", console.log);
+  await qu.start();
+  await qu.pubsub.start();
+  return qu;
+};
